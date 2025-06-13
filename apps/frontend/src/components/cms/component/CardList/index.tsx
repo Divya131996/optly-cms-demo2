c1fef1ca84401923a46ec0f45dff7fd62bbffcff
Divya Patel <Divya.P@symsoftsolutions.com>
2025-06-13 13:13:55 -0700

    card list  component changes

--- apps/frontend/src/components/cms/component/CardList/index.tsx
+++ apps/frontend/src/components/cms/component/CardList/index.tsx
@@ -1,39 +1,20 @@
-import { type CmsComponent } from "@remkoj/optimizely-cms-react"
-import { CardListDataFragmentDoc, type CardListDataFragment } from "@/gql/graphql"
+import { type CmsComponent } from "@remkoj/optimizely-cms-react";
+import { CardListDataFragmentDoc, type CardListDataFragment } from "@/gql/graphql";
 
-export const CardListComponent: CmsComponent<CardListDataFragment> = ({ data, children }) => {
-    const title = data?.Title ?? "Untitled"
-    const description = data?.Description ?? ""
-
-    // Access the internal image block (ImageElement)
-    const imageBlock = data?.Image?.content?.[0]
-
-    const imageUrl = imageBlock?.imageLink?.url ?? ""
-    const imageAlt = imageBlock?.altText ?? "Image"
-
-    return (
-        <div className="w-full border-y border-y-solid border-y-slate-900 py-4 mb-4 px-4">
-            <div className="bg-white p-4 rounded-md shadow border border-slate-300 flex flex-col sm:flex-row gap-4">
-                {imageUrl && (
-                    <div className="flex flex-col sm:w-40">
-                        <img
-                            src={imageUrl}
-                            alt={imageAlt}
-                            className="w-full h-auto object-cover rounded-md border border-slate-200"
-                        />
-                    </div>
-                )}
-                <div>
-                    <h3 className="text-xl font-semibold mb-2 text-slate-800">{title}</h3>
-                    <p className="text-base text-slate-600">{description}</p>
-                </div>
-            </div>
-
-            {children && <div className="mt-4 mx-2">{children}</div>}
-        </div>
-    )
+/**
+ * CardList
+ * 
+ */
+export const CardListComponent : CmsComponent<CardListDataFragment> = ({ data, children }) => {
+    const componentName = 'CardList'
+    const componentInfo = ''
+    return <div className="w-full border-y border-y-solid border-y-slate-900 py-2 mb-4">
+        <div className="font-bold italic">{ componentName }</div>
+        <div>{ componentInfo }</div>
+        { Object.getOwnPropertyNames(data).length > 0 && <pre className="w-full overflow-x-hidden font-mono text-sm bg-slate-200 p-2 rounded-sm border border-solid border-slate-900 text-slate-900">{ JSON.stringify(data, undefined, 4) }</pre> }
+        { children && <div className="mt-4 mx-4 flex flex-col">{ children }</div>}
+    </div>
 }
-
 CardListComponent.displayName = "CardList (Component/CardList)"
 CardListComponent.getDataFragment = () => ['CardListData', CardListDataFragmentDoc]
 
